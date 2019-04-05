ce14d364e5548ccf4293b33484390f850ae9489b
graesonb123 <bullingtong@health.missouri.edu>
2019-04-05 17:25:05 -0500

    First commit messed up first

---
+++ BullingtongColorNav/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullingtongColorNav
+//
+//  Created by Graeson Bullington on 4/5/19.
+//  Copyright © 2019 Graeson Bullington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
