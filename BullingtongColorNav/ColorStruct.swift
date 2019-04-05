a5fe74ff5387ce9239acce945fe4fe7ce9e2c3a8
graesonb123 <bullingtong@health.missouri.edu>
2019-04-05 17:52:41 -0500

    Redid first assignment because I did it wrong and added the background to the colors

---
+++ BullingtongColorNav/ColorStruct.swift
@@ -0,0 +1,14 @@
+//
+//  ColorStruct.swift
+//  BullingtongColorNav
+//
+//  Created by Graeson Bullington on 4/5/19.
+//  Copyright © 2019 Graeson Bullington. All rights reserved.
+//
+
+import UIKit
+
+struct Colors{
+    let name: String
+    let uiColor: UIColor
+}
---
+++ BullingtongColorNav/ColorViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ColorViewController.swift
+//  BullingtongColorNav
+//
+//  Created by Graeson Bullington on 4/5/19.
+//  Copyright © 2019 Graeson Bullington. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    //var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    var colors = [Colors(name: "red", uiColor: UIColor.red),
+                  Colors(name: "orange", uiColor: UIColor.orange),
+                  Colors(name: "yellow", uiColor: UIColor.yellow),
+                  Colors(name: "green", uiColor: UIColor.green),
+                  Colors(name: "blue", uiColor: UIColor.blue),
+                  Colors(name: "purple", uiColor: UIColor.purple),
+                  Colors(name: "brown", uiColor: UIColor.brown)
+    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        cell.textLabel?.text = colors[indexPath.row].name
+        cell.backgroundColor = colors[indexPath.row].uiColor
+        
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
